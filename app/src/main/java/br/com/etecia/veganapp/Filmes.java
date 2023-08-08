30e358da905aa680bc5089769844cbb1a255c9ab
JoaoVictor77703 <joao@nurog.com.br>
2023-08-08 20:16:04 -0300

    atualiza1.0

--- app/src/main/java/br/com/etecia/veganapp/Filmes.java
+++ app/src/main/java/br/com/etecia/veganapp/Filmes.java
@@ -18,4 +18,20 @@ public class Filmes {
         this.categoria = categoria;
         this.imagem = imagem;
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescriçao() {
+        return descriçao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
 }
