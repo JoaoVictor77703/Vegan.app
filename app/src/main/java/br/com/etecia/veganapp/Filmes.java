3f3677c9617c1b2419fa5bba0d060cf763870603
JoaoVictor77703 <joao@nurog.com.br>
2023-08-08 20:14:25 -0300

    atualizar1

---
+++ app/src/main/java/br/com/etecia/veganapp/Filmes.java
@@ -0,0 +1,21 @@
+package br.com.etecia.veganapp;
+
+public class Filmes {
+    private String titulo;
+
+    private String descriçao;
+
+    private String categoria;
+
+    private String imagem;
+
+    //criar o contrutor comparando alt+insert
+
+
+    public Filmes(String titulo, String descriçao, String categoria, String imagem) {
+        this.titulo = titulo;
+        this.descriçao = descriçao;
+        this.categoria = categoria;
+        this.imagem = imagem;
+    }
+}
